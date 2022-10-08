21736b82973f4c376930b75270a53f7c48b9e4cb
Mervin Hemaraju <th3pl4gu33@gmail.com>
2022-10-08 18:33:02 +0400

    - Updated setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r", encoding="utf-8") as rm:
 
 setup(
     name="aws-snappy",
-    version="2.0",
+    version="2.1.0",
     author="Mervin Hemaraju",
-    author_email="mervin.hemaraju@checkout.com",
+    author_email="th3pl4gu33@gmail.com",
     description="Snappy is a Python library that facilitates taking root volume snapshots from EC2 as well as other EBS volumes.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mervin-hemaraju-cko/snappy",
+    url="https://github.com/mervinhemaraju/snappy",
     packages=find_packages(exclude=('tests')),
     install_requires=[
           'boto3'
